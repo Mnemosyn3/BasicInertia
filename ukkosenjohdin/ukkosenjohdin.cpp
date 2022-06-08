946d7fec19963c68b17a12d97ec28f5ec5355707
Mnemosyn3 <robert.hamalainen@iki.fi>
2022-06-08 13:28:00 +0300

    preliminary menu

--- ukkosenjohdin/ukkosenjohdin.cpp
+++ ukkosenjohdin/ukkosenjohdin.cpp
@@ -1,59 +1,36 @@
-#include <stdlib.h>
-#include <stdio.h>
+#include <iostream>
+using namespace std;
 
-#ifdef _WIN32
-#include <Windows.h>
-#else
-#include <unistd.h>
-#endif
-
-#include "rs232.h"
-
-
+#include <libserialport.h>
+bool run = true;
 
 int main()
 {
-  int i, n,
-      cport_nr=16,        /* /dev/ttyS0 (COM1 on windows) */
-      bdrate=19200;       /* 19200 baud */
+while (run){
+  int selection;
+  cout << "Ukkosenjohdin v0.0.1\nMenu:\n0: Exit\n1: Select serial device\n2:Start test\n";
+  
+  cin >> selection;
+  
 
-  unsigned char buf[4096];
-
-  char mode[]={'8','N','1',0};
-
-
-  if(RS232_OpenComport(cport_nr, bdrate, mode,1))
+  switch (selection)
   {
-    printf("Can not open comport\n");
+  case 0:
+    run = false;
+    break;
 
-    return(0);
+  case 1:
+    cout <<"test 1";
+    break;
+  
+  case 2:
+    cout <<"test2";
+  
+  default:
+    cout <<"Selection not recocnised\n";
+    break;
   }
+}
 
-  while(1)
-  {
-    n = RS232_PollComport(cport_nr, buf, 4095);
-
-    if(n > 0)
-    {
-      buf[n] = 0;   /* always put a "null" at the end of a string! */
-
-      for(i=0; i < n; i++)
-      {
-        if(buf[i] < 32)  /* replace unreadable control-codes by dots */
-        {
-          buf[i] = '.';
-        }
-      }
-
-      printf("received %i bytes: %s\n", n, (char *)buf);
-    }
-
-#ifdef _WIN32
-    Sleep(100);
-#else
-    usleep(100000);  /* sleep for 100 milliSeconds */
-#endif
-  }
-
-  return(0);
+return 0;
 }
